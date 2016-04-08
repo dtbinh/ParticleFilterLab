57588cfdc0e5f61445d5bab3e9c480401134bd91
Jerry Webb <jerryweb@usc.edu>
2016-04-07 19:44:39 -0700

    Resampling doesn't think it sums to 1

--- code/lab10.py
+++ code/lab10.py
@@ -21,7 +21,13 @@ class Run:
         self.odometry = odometry.Odometry()
         self.theta = 0
         self.particleFilter = particle_filter.ParticleFilter()
-        self.data = [0.5, 0.5, 0.1, math.pi/2,1.5, 1, 0.1, 0]
+        self.data = []
+        for i in range(0, 100,1):
+            # self.data = [self.particleFilter.randomNumbers[i]/100, self.particleFilter.randomNumbers[i+1]/100, 0.1, self.particleFilter.randomTheta[i],]
+            self.data.append(self.particleFilter.particles[i].x)
+            self.data.append(self.particleFilter.particles[i].y)
+            self.data.append(self.particleFilter.particles[i].z)
+            self.data.append(self.particleFilter.particles[i].theta)
 
 
 
@@ -46,7 +52,8 @@ class Run:
             self.time.sleep(motorSleepTime)
             self.create.drive_direct(0,0)
 
-        self.particleFilter.recieveCommand(self.theta, 0.5)
+        # distance = self.map.closest_distance((self.particleFilter.randomNumbers[0]/100, self.particleFilter.randomNumbers[1]/100), self.particleFilter.randomTheta[0])
+        self.particleFilter.recieveCommand(self.theta, 0.5, self.getSensorData())
         self.updateDataForParticles()
 
 
@@ -74,15 +81,19 @@ class Run:
     def getSensorData(self):
         reading = self.sonar.get_distance()
         print("Sense pressed! Reading is: ", reading)
-        self.particleFilter.recieveCommand(0,reading)
+        # self.particleFilter.recieveCommand(0,reading)
+        return reading
 
 
     def updateDataForParticles(self):
         del self.data[:]
-        for i in range(0,2,1):
-            for j in range(0,4,1):
-                self.data.append(self.particleFilter.particles[i][j])
-                print(self.particleFilter.particles[i][j])
+        for i in range(0, 100,1):
+            # self.data = [self.particleFilter.randomNumbers[i]/100, self.particleFilter.randomNumbers[i+1]/100, 0.1, self.particleFilter.randomTheta[i],]
+            self.data.append(self.particleFilter.particles[i].x)
+            self.data.append(self.particleFilter.particles[i].y)
+            self.data.append(self.particleFilter.particles[i].z)
+            self.data.append(self.particleFilter.particles[i].theta)
+
         self.virtual_create.set_point_cloud(self.data)
 
 
@@ -93,9 +104,8 @@ class Run:
 
         # This is an example on how to show particles
         # the format is x,y,z,theta,x,y,z,theta,...
-
+        # self.virtual_create.set_point_cloud(self.data)
         self.virtual_create.set_point_cloud(self.data)
-
         # This is an example on how to estimate the distance to a wall for the given
         # map, assuming the robot is at (0, 0) and has heading math.pi
 
--- code/particle_filter.py
+++ code/particle_filter.py
@@ -4,64 +4,100 @@ import random
 from array import *
 import numpy as np
 
+class Particle:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0.1
+        self.theta = 0
+        self.actualWeight = 0
 
 class ParticleFilter:
     def __init__(self):
         self.commandGiven = False
-        # self.particles = array()
+        self.map = lab10_map.Map("lab10.map")
+
         self.muTheta = 0
         self.sigma = 0.05
-        self.muD = 0.5  #mean
+        self.muD = 0.5  # mean
         self.scale = 100
+        self.previousProbability = 1 / 36
 
         self.randomNumbers = random.sample(range(300), 200)
-        self.randomTheta = random.sample(range(360), 100)
-        initialTuple = (self.randomNumbers[0]/self.scale, self.randomNumbers[1]/self.scale, self.randomNumbers[2]/self.scale, self.randomTheta[0])
+        self.randomTheta = random.sample(range(360), 200)
 
-        print(self.randomNumbers[0]/100)
-        self.particles = [initialTuple]
+        self.particles = []
+        self.particleWeights = []
 
-        for i in range(0,1,1):
-            tuple = (self.randomNumbers[3]/self.scale, self.randomNumbers[4]/self.scale, self.randomNumbers[5]/self.scale, self.randomTheta[1])
-            self.particles.append(tuple)
+        for i in range(0, 200, 2):
+            particle = Particle()
+            particle.x = self.randomNumbers[i]/100
+            particle.y = self.randomNumbers[i+1]/100
+            particle.theta = self.randomTheta[i]
+            # tuple = (, self.randomNumbers[i + 1] / 100, 0.1, self.randomTheta[i])
+            self.particles.append(particle)
 
     # x = fake sensor reading, mu = actual sensor reading
-    def findPDF(self, x ,mu ,sigma):
-        probability = (1/(sigma * math.sqrt(2*math.pi)))*math.exp(-((x-mu)^2)/(2*(sigma^2)))
+    def findPDF(self, x, mu, sigma):
+        probability = (1 / (sigma * math.sqrt(2 * math.pi))) * math.pow(math.exp(-(x - mu)), 2) / (
+            2 * math.pow(sigma, 2))
         return probability
 
-
-    def computeParticleD(self, scale, muD, muTheta, sigma, x):
+    def computeParticleD(self, scale, muD, muTheta, sigma, x, sensorReading, weightSum):
         # for each particle
-        self.findPDF(x,muD,sigma)
+        conditional_probabilty = self.findPDF(x, sensorReading, sigma)
+        weight = conditional_probabilty * self.previousProbability
+        return weight / weightSum
+
+
+
         # need to find the probability of a particle at each position given a sensor reading at that postition (use the map.closest_distance)
 
-
-        # for i in range(0, 2,1):
-        #     d = np.random.normal(muD, scale, sigma)
-        #     theta = np.random.normal(muTheta, scale, sigma)
-        #     # generate x and y positions
-        #     initialX = self.particles[i][0]
-        #     print("initial theta", muTheta)
-        #
-        #     initialY = self.particles[i][1]
-        #     initialZ = self.particles[i][2]
-        #     initialTheta = self.particles[i][3]
-        #     value = d * math.cos(initialTheta + theta)
-        #     value2 = d * math.cos(initialTheta + theta)
-        #
-        #
-        #     self.particles[i][0] = initialX + value #d*np.cos(initialTheta + theta)
-        #     self.particles[i][1] = initialY + value2 #d*np.cos(initialTheta + theta)
-        #     self.particles[i][2] = initialZ
-        #     self.particles[i][3] = theta
-
-
-
-    def recieveCommand(self, _muTheta, _muDistance):
+    def recieveCommand(self, _muTheta, _muDistance, sensorReading):
         # 90 = turn right, -90 = turn left, 0 = move forward
         self.muTheta = _muTheta
         self.muD = _muDistance
         print("recieved command")
-        self.commandGiven = True
-        self.computeParticleD(self.scale, self.muD, self.muTheta, self.sigma)
+        # self.commandGiven = True
+        weightSum = 0
+        actualWeight = 0
+        particle_sensor_reading_array = []
+
+        # create particle sensor readings and store into an array
+        for i in range(0, 100, 1):
+            # self.particles[i].weight = self.map.closest_distance((self.particles[i].x, self.particles[i].y), self.particles[i].the
+            #                                                      )
+            # = self.map.closest_distance((self.particles[i].x, self.particles[i].y), self.particles[i].theta))
+            particle_sensor_reading_array.append(
+                    self.map.closest_distance((self.particles[i].x, self.particles[i].y), self.particles[i].theta))
+
+        # calculate the weightSum of all the particles
+        for i in range(0, 100, 1):
+            weightSum += self.computeParticleD(self.scale, self.muD, self.muTheta, self.sigma, sensorReading,
+                                               particle_sensor_reading_array[i], 1)
+
+        print("weight sum = ", weightSum)
+
+        # calculate the actual weights of each particle
+        del self.particleWeights[:]
+        for i in range(0, 100, 1):
+            actualWeight += self.computeParticleD(self.scale, self.muD, self.muTheta, self.sigma, sensorReading,
+                                                  particle_sensor_reading_array[i], weightSum)
+            self.particles[i].weight = actualWeight
+            self.particleWeights.append(actualWeight)
+
+        print("weight sum = ", actualWeight)
+
+        self.resampleParticles()
+
+    # //Use a roulette wheel to probabilistically duplicate particles with high weights,
+    # //and discard those with low weights. A ‘Particle’ is some structure that has
+    # //a weight element w. The sum of all w’s in oldParticles should equal 1.
+    def resampleParticles(self):
+    #     # new_particles = []
+    #     # cdf = []
+    #     # for i in range(0, 100, 1):
+    #     #     new_particles.append(self.particles[i])
+    #     #     cdf.append(cdf)
+        np.random.choice(self.particles, 100, p=self.particleWeights)
+
